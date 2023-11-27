36f6ece96926c0af6071613e997f0784e40a704f
grc02 <georgi.chonkov27@gmail.com>
2023-11-27 16:01:10 +0200

    Added simple for interfaces for depositing into pools of protocols such as 'Uniswap' & 'Aave'

---
+++ src/interfaces/ILendingPool.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+interface ILendingPool {
+    function deposit(address asset, uint256 amount, address onBehalfOf, uint16 referralCode) external;
+}
